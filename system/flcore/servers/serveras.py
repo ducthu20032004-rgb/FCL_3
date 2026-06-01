15204def0d614cac8c4619226bcc083e293fee38
Thu Tran <ducthu20032004@gmail.com>
2026-06-01 21:58:32 +0700

    fedas kaggle

--- system/flcore/servers/serveras.py
+++ system/flcore/servers/serveras.py
@@ -322,7 +322,7 @@ class FedAS(Server):
                         pass   
                     if client.id in [0, 1,2,3,4]:  
                         try:
-                            save_dir =  "C:/Thu/FCL/weight_fedas/weight_client_round"  # Thay đổi đường dẫn theo nhu cầu
+                            save_dir =  "/kaggle/working/FCL_3/weight_fedas/weight_client_round"  # Thay đổi đường dẫn theo nhu cầu
                             os.makedirs(save_dir, exist_ok=True)
                             save_path = f"{save_dir}client_{client.id}_task_{task}_round_{i}.pt"
                             
@@ -481,7 +481,7 @@ class FedAS(Server):
                         import traceback; traceback.print_exc()   
 
 
-                    csv_path = "C:/Thu/FCL_3/weight_fedas/fedas_drift_results.csv"
+                    csv_path = "/kaggle/working/FCL_3/weight_fedas/fedas_drift_results.csv"
                     os.makedirs(os.path.dirname(csv_path), exist_ok=True)
                     fieldnames = ["round", "task", "client", "block",
                                 "drift_trained", "drift_aggre", "drift_global",
